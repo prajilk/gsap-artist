c14dfdcc285af3eb2409c8620736cc53e9274f17
Prajil K <prajilk20017@gmail.com>
2024-11-14 23:12:12 +0530

    downgraded react and replace lenis code

--- src/app/provider/lenis.tsx
+++ src/app/provider/lenis.tsx
@@ -1,19 +1,13 @@
 "use client";
 
-import Lenis from "lenis";
-import { useEffect } from "react";
+import { ReactLenis } from "lenis/react";
 
-const LenisProvider = ({ children }: { children: React.ReactNode }) => {
-    useEffect(() => {
-        const lenis = new Lenis(); // initialize Lenis
+function SmoothScrolling({ children }: { children: React.ReactNode }) {
+    return (
+        <ReactLenis root options={{ lerp: 0.1, duration: 1.5 }}>
+            {children}
+        </ReactLenis>
+    );
+}
 
-        function raf(time: number) {
-            lenis.raf(time);
-            requestAnimationFrame(raf);
-        }
-        requestAnimationFrame(raf);
-    }); // if in nextjs, completely ommit the 2nd argument of useEffect i.e. []
-
-    return <>{children}</>; // make sure you don't pass plain children, it has to be html
-};
-export default LenisProvider;
+export default SmoothScrolling;
